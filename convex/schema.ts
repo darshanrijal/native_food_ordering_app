07702aa5334f57bd3dde59e1950b0db871076013
darshan <darshanrijal0@gmail.com>
2025-07-19 15:00:40 +0545

    add customizations database tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -8,4 +8,36 @@ export default defineSchema({
     password: v.string(),
     avatarUrl: v.optional(v.string()),
   }).index("email_uniq_idx", ["email"]),
+  categories: defineTable({
+    name: v.string(),
+    description: v.string(),
+  }),
+  menu: defineTable({
+    name: v.string(),
+    description: v.string(),
+    imageUrl: v.string(),
+    rating: v.float64(),
+    calories: v.int64(),
+    protien: v.int64(),
+    price: v.float64(),
+    categoryId: v.id("categories"),
+  }).index("category_id_fk", ["categoryId"]),
+  customization: defineTable({
+    name: v.string(),
+    price: v.int64(),
+    value: v.union(
+      v.literal("topping"),
+      v.literal("side"),
+      v.literal("size"),
+      v.literal("crust"),
+      v.literal("bread"),
+      v.literal("spice"),
+      v.literal("base"),
+      v.literal("sauce"),
+    ),
+  }),
+  menuCustomizations: defineTable({
+    menuId: v.id("menu"),
+    customizationId: v.id("customization"),
+  }).index("fks", ["menuId", "customizationId"]),
 });
